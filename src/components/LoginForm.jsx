c44570daa901ae306f6183c7c106b05e5467ac1c
camilaalbuq <cam.albuquer@gmail.com>
2024-05-24 10:35:12 +0200

    adjusted home

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,86 @@
+import React from "react"
+
+function LoginForm() {
+    return (
+        <form noValidate="" action="" className="Form w-1/2">
+            <div className="min-h-1/2 flex w-full flex-col justify-center bg-gray-100 px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                    <img
+                        className="mx-auto h-12 w-auto"
+                        src={logo}
+                        alt="Workflow"
+                    />
+                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                        Create your account
+                    </h2>
+                    <p className="max-w mt-2 text-center text-sm text-gray-600">
+                        Already registered? {""}
+                        <a
+                            href="#"
+                            className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                        >
+                            Sign in
+                        </a>
+                    </p>
+                </div>
+
+                <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                    <div className="rounded-lg bg-white px-6 py-8 shadow sm:px-10">
+                        <form class="mb-0 space-y-6" action="#" method="POST">
+                            <div>
+                                <label
+                                    htmlFor="email"
+                                    className="block text-sm font-medium text-gray-700"
+                                >
+                                    Email
+                                </label>
+                                <div className="mt-1">
+                                    <input
+                                        id="email"
+                                        name="email"
+                                        type="email"
+                                        placeholder="email@littleanimals.com"
+                                        autocomplete="email"
+                                        required
+                                        className="mb-4 w-full rounded-md border-2 border-blue-600 px-4 py-2 focus:ring dark:border-gray-300 focus:dark:ring-indigo-600"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <label
+                                    htmlFor="password"
+                                    className="block text-sm font-medium text-gray-700"
+                                >
+                                    Password
+                                </label>
+                                <div className="mt-1">
+                                    <input
+                                        id="password"
+                                        name="password"
+                                        type="password"
+                                        autocomplete="current-password"
+                                        placeholder="exampleNum&Letters123"
+                                        required
+                                        className="mb-4 w-full rounded-md border-2 border-blue-600 px-4 py-2 focus:ring dark:border-gray-300 focus:dark:ring-indigo-600"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <button
+                                    type="submit"
+                                    className="flex w-full justify-center rounded-md border border-transparent bg-green-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                >
+                                    Login
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default LoginForm
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom"
 
 function Sidebar() {
     return (
-        <div className="sidebar w-full p-3 text-base text-black">
-            <div className="flex flex-row items-center justify-center space-x-8">
+        <div className="sidebar roudned-b-full w-full bg-orange-300 p-3 text-base text-black">
+            <div className="roudned-b-full flex flex-row items-center justify-center space-x-8">
                 <ul className="flex flex-row space-x-8">
                     <li>
                         <Link
